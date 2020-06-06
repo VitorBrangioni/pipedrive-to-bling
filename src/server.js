1dab9ce40709b1f788d6e1ed38a8acc82fe57243
Vitor Brangioni <vitorh.brangioni@gmail.com>
2020-06-06 15:59:21 -0300

    Changed api port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3333));
 
 app.listen(app.get('port'), function () {
 	console.log('Node app is running on port', app.get('port'));
